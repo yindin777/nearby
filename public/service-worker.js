ff0f6e6ce3592f32741fabcc5b58df1b9fb5e119
Yindin <yindin777@gmail.com>
2024-12-08 10:11:17 +0100

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'nearby-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/src/js/app.js',
+    '/src/css/tailwind.css',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
